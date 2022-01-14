0b2b0d7864c26ab65fb282aced4ab87eb7bb726b
Varnika Rathee <77229404+varnikarathee@users.noreply.github.com>
2022-01-14 20:51:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
+const navToggleBtn = document.querySelector('.navbar-toggler');
+const navCollapseDiv = document.querySelector('.navbar-collapse');
 
-        var popupViews = document.querySelectorAll('.popup_view');
+navToggleBtn.addEventListener('click', () => {
+    navCollapseDiv.classList.toggle('showNavbar');
+    navToggleBtn.classList.toggle('showNavbar-toggler');
+});
+
+let resizeTimer;
+window.addEventListener('resize', () => {
+    document.body.classList.add('resize-animation-stopper');
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(() => {
+        document.body.classList.remove('resize-animation-stopper');
+    }, 400);
+});
+        
+          var popupViews = document.querySelectorAll('.popup_view');
         var popupBtns = document.querySelectorAll('.popup_btn');
         var closeBtns = document.querySelectorAll('.close_btn');
 
